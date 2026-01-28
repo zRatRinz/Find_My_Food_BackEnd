15b96fc7a44165418706b5d853752b513f7029ec
Ratthakit Kotcharin <ratthakit.kcr@gmail.com>
2026-01-29 01:16:29 +0700

    🚑️ ทดลองปรับ SSL Close

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,14 @@
 from sqlmodel import create_engine, Session
 from app.core.config import DBURL
 
-engine = create_engine(DBURL, echo=True)
+engine = create_engine(
+    DBURL,
+    echo=False,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=5,
+    pool_recycle=300,
+)
 
 def get_db():
     with Session(engine) as db:
